740d015ee40d442f2d568e02cef608be8d0022f7
EdNeu123 <eduardoneumannpb@gmail.com>
2025-04-12 01:11:59 -0300

    visualizar pagina de detalhes

--- buscar_api.js
+++ buscar_api.js
@@ -1,18 +1,22 @@
 function buscar_api() {
-    const id = document.getElementById('id').value;
-    fetch('https://rickandmortyapi.com/api/character/'+id) //requere a api
+    const id = document.querySelector('#id').value.trim();
+    if (!id) {
+        alert('Por favor, insira um ID válido e não nulo.');
+        return;
+    }
+    localStorage.setItem('selectedId', id);
+        fetch('https://rickandmortyapi.com/api/character/'+id) //requere a api
         .then(response => response.json()) //transforma em json
         .then(data => {
-            const tabela = document.getElementById('fact');
+            const tabela = document.querySelector('#Rick');
             tabela.innerHTML = ' '; //limpa a tabela antes de preencher
             const linha = ` <tr>
                 <td>${data.id}</td>
                 <td>${data.name}</td>
                 <td>${data.species}</td>
-                <td>${data.type}</td>
+                <td>${data.type  || 'Sem Informação' }</td>
                 </tr> `;
             tabela.innerHTML = linha;
-
         })
         .catch(error => console.log('Erro ao consumir a API: ' + error));
 }
--- consumir_api.js
+++ consumir_api.js
@@ -1,14 +1,22 @@
+
 function consumoAPI() {
-    fetch('https://rickandmortyapi.com/api/character/'+Math.floor(Math.random() * 826)) //requere a api
+    const id = localStorage.getItem('selectedId');
+    if (!id) {
+        console.error('Nenhum ID selecionado. Volte à página de busca.');
+        return;
+    }
+    fetch('https://rickandmortyapi.com/api/character/'+id) //requere a api
         .then(response => response.json()) //transforma em json
         .then(data => {
-            const tabela = document.getElementById('fact');
+            const tabela = document.getElementById('Rick2');
             tabela.innerHTML = ' '; //limpa a tabela antes de preencher
             const linha = ` <tr>
+                <td><img src="${data.image}"></td>
                 <td>${data.id}</td>
                 <td>${data.name}</td>
                 <td>${data.species}</td>
                 <td>${data.type}</td>
+                <td>${data.gender}</td>
                 </tr> `;
             tabela.innerHTML = linha;
 
